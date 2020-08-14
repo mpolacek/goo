1a34eb7b0c7a62f77a221bea6cce2f0a0c947af4
Marek Polacek <polacek@redhat.com>
2020-08-14 17:52:59 -0400

    Add gcm.cc.

---
+++ gcm.cc
@@ -0,0 +1,40 @@
+#include <cstddef>
+#include <utility>
+
+// Recursive Remainder Lemma:
+// If r = segment_remainder (a, 2b), then
+//
+// segment_remainder (a, b) =
+// 1) r  if  r <= b
+// 2) r - b  if  r > b
+
+size_t
+rem (size_t a, size_t b)
+{
+  if (a <= b)
+    return a;
+  if (a - b <= b)
+    return a - b;
+  a = rem (a, 2 * b);
+  if (a <= b)
+    return a;
+  return a - b;
+}
+
+size_t
+gcm (size_t a, size_t b)
+{
+  while (a != b)
+    {
+      a = rem (a, b);
+      std::swap (a, b);
+    }
+  return a;
+}
+
+int
+main ()
+{
+  if (gcm (45, 6) != 3)
+    __builtin_abort ();
+}
